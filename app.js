8fa84d872a7216e317aaf2a267dd71089a3fddd2
grassfinn <theharper@live.com>
2022-08-05 23:11:22 -0400

    API Blog Code

---
+++ app.js
@@ -0,0 +1,24 @@
+const fox = document.getElementById('fox');
+
+const backupImg = {
+  image: 'https://randomfox.ca/images/47.jpg',
+  link: 'https://randomfox.ca/?i=47',
+};
+
+async function getFox() {
+  const response = await fetch('https://randfox.ca/floof/?ref=apilist.fun');
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+getFox()
+  .then((item) => {
+    fox.setAttribute('src', item.image);
+  })
+  .catch((err) => {
+    alert('Something went wrong! Enjoy this picture of a fox instead!');
+    // set up default image
+    console.error(err);
+    fox.setAttribute('src', backupImg.image);
+  });
